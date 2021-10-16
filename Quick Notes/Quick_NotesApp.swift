642a74d4b7d9f3bcd0bdaceecec776a1fb3d2b1b
Drew Johnson <dtjohnson1995@gmail.com>
2021-10-16 19:39:59 -0400

    Initial Commit

---
+++ Quick Notes/Quick_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_NotesApp.swift
+//  Quick Notes
+//
+//  Created by Drew Johnson on 10/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Quick_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
